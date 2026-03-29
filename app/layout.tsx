cf3f3d68aa7e7172e52327d8fd6431ec846fd9ad
prajwallshetty <prajwal0shetty11@gmail.com>
2026-03-29 15:13:54 +0530

    Add TypeScript configuration file (tsconfig.json) for project setup

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "AAKAR - A J Institute of Engineering & Technology, Mangaluru",
+  description: "AAKAR is the annual technical fest of A J Institute of Engineering & Technology, Mangaluru. It is a platform for students to showcase their technical skills and creativity through various events and competitions.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
