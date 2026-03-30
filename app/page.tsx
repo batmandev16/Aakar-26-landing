bc3245a81718c9d202bebc7cc426dcf697cd5888
prajwallshetty <prajwal0shetty11@gmail.com>
2026-03-30 09:12:20 +0530

    feat: remove EnterScreen component and simplify Home component rendering

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,13 @@
 
 import { useState } from "react";
 import HeroGate from "../components/Herogate";
-import EnterScreen from "../components/EnterScreen";
-
 
 export default function Home() {
   const [entered, setEntered] = useState(false);
 
   return (
     <>
-      {!entered ? (
-        <EnterScreen onEnter={() => setEntered(true)} />
-      ) : (
         <HeroGate />
-      )}
     </>
   );
 }
